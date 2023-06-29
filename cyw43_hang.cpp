da980669715835c0390bebfcb7ae98a03d1442a9
Michael Giglia <michael.a.giglia@gmail.com>
2023-06-29 09:46:04 -0400

    removed print in isr

--- cyw43_hang.cpp
+++ cyw43_hang.cpp
@@ -22,7 +22,7 @@ const uint32_t isr_events
 
 void isr(void)
 {
-    cout << "isr activated" << '\n';
+    // cout << "isr activated" << '\n';
     if(gpio_get_irq_event_mask(lpin) & isr_events)
     {gpio_acknowledge_irq(lpin, isr_events);
         countl++;
@@ -35,6 +35,7 @@ void isr(void)
 
 void init_isr(void)
 {
+    irq_set_enabled(IO_IRQ_BANK0, false);
 	gpio_set_input_enabled(lpin, true);
     gpio_set_irq_enabled(lpin, isr_events, true);
     gpio_add_raw_irq_handler(lpin, &isr);
@@ -50,19 +51,19 @@ int main()
     init_isr();
 
     sleep_ms(1500);
-
-
-    
+    cout << "Starting..." << '\n';
     if(cyw43_arch_init())
     {
         cout << "CYW43 INIT failed!" << '\n';
     }
+
     cout << "init worked!" << '\n';
     
     cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
     
     while(true)
     {   
+        cyw43_arch_poll();
         sleep_ms(100);
         // cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, !(cyw43_arch_gpio_get(0)));
         cout << "left: " << countl << " right: " << countr;
